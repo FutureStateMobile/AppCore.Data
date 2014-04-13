c93dd1312abd30b239177c7940d499ff34cd178d
Keith Larsen <keithnlarsen@gmail.com>
2014-04-13 09:29:56 -0600

    Added a new JoinExpressionVisitor test.

---
+++ FutureState.AppCore.Tests/Unit/JoinExpressionVisitorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using FutureState.AppCore.Data;
+using FutureState.AppCore.Tests.Helpers.Models;
+using NUnit.Framework;
+
+namespace FutureState.AppCore.Tests.Unit
+{
+    [TestFixture]
+    public class JoinExpressionVisitorTests
+    {
+        public static JoinExpressionVisitor JoinExpression<TModel, TJoinTo> ( JoinType joinType, Expression<Func<TModel, TJoinTo, object>> joinExpression ) where TModel : class, new()
+        {
+            var visitor = new Data.JoinExpressionVisitor().Visit(joinType, joinExpression );
+            return visitor;
+        }
+
+        [Test, Ignore]
+        public void ShouldBuildTheJoinExpression ()
+        {
+            // Setup
+            const string expectedString = "INNER JOIN Students.Id == Books.StudentId";
+            var actualExpression = JoinExpression<StudentModel, BookModel>( JoinType.Inner, (s, b) => s.Id == b.StudentId );
+            
+            // Execute
+            var actualString = actualExpression.JoinExpression;
+
+            // Test
+            Assert.AreEqual( expectedString, actualString );
+        }
+
+    }
+}

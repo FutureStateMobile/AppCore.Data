f838793877cdcef18015ac3e955234f20cb48ef5
Chase Florell <chase.florell@gmail.com>
2014-04-16 18:19:21 -0600

    Added iOS (Touch) project

---
+++ FutureState.AppCore.Data.Sqlite.Touch/IDbConnectionProvider.cs
@@ -0,0 +1,9 @@
+﻿using Mono.Data.Sqlite;
+
+namespace FutureState.AppCore.Data.Sqlite.Touch
+{
+    public interface IDbConnectionProvider
+    {
+        SqliteConnection GetOpenConnection();
+    }
+}

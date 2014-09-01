9d95b449e2396a9aa0895efc98186e968bcec6ab
Chase Florell <chase.florell@gmail.com>
2014-08-31 19:17:32 -0600

    Added new exception

---
+++ FutureState.AppCore.Data/Exceptions/DuplicateRecordException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FutureState.AppCore.Data.Exceptions
+{
+    public class DuplicateRecordException : Exception
+    {
+        public DuplicateRecordException()
+        {
+        }
+
+        public DuplicateRecordException(string message) : base(message)
+        {
+        }
+    }
+}

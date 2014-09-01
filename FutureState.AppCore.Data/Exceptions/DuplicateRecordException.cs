eadb490ca8951123f371c9de0dcfd03c31394a14
Chase Florell <ChaseFlorell@users.noreply.github.com>
2014-08-31 19:06:24 -0600

    Create DuplicateRecordException.cs

---
+++ FutureState.AppCore.Data/Exceptions/DuplicateRecordException.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace FutureState.AppCore.Data.Exceptions
+
+    public class DuplicateRecordException : Exception
+    {
+        public DuplicateRecordException()
+        {
+        }
+
+        public DuplicateRecordException(string message) : base(message)
+        {
+        }
+    }
+}

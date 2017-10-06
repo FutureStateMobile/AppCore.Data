edfe33700cefd56d586236a126742971779caeaa
rocki <chase.florell@gmail.com>
2017-10-06 13:32:25 -0700

    [CF] made it so that generics would still build property names correctly

---
+++ FutureState.AppCore.Data.Tests/Helpers/Fixtures/GenericFixture.cs
@@ -0,0 +1,10 @@
+﻿namespace FutureState.AppCore.Data.Tests.Helpers.Fixtures
+{
+    public class Foo<TFoo,TBar,TBaz>
+    {
+        public TFoo Item { get; set; }
+        public TBar Bar { get; set; }
+        public TBaz Baz { get; set; }
+    }
+    public class Bar { }
+}

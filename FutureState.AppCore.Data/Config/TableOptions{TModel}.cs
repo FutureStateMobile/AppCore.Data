a8b3c96165d59faa789dec808adbbec1e0310fd4
rocki <chase.florell@gmail.com>
2017-11-27 07:47:19 -0800

    [CF] while loop was invalid

--- FutureState.AppCore.Data/Config/TableOptions{TModel}.cs
+++ FutureState.AppCore.Data/Config/TableOptions{TModel}.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Linq.Expressions;
-using FutureState.AppCore.Data.Exceptions;
 
 namespace FutureState.AppCore.Data.Config
 {
     public class TableOptions<TModel> where TModel : class, new()
     {
-        private const int _maxMemberExpressionTries = 3;
         private readonly DbProviderBase _dbProvider;
 
         public TableOptions(IDbProvider dbProvider)
@@ -15,19 +13,15 @@ namespace FutureState.AppCore.Data.Config
         }
 
         /// <summary>
-        ///     Tells the DbProvider what the primary key is for <see cref="TModel" />
+        ///     Tells the DbProvider what the PrimaryKey is for <see cref="TModel" />
         /// </summary>
         public TableOptions<TModel> SetPrimaryKey(Expression<Func<TModel, object>> func)
         {
             var body = func.Body as MemberExpression;
-            var count = 0;
-            while (body == null)
+            if (body == null)
             {
                 var ubody = (UnaryExpression) func.Body;
-                body = ubody.Operand as MemberExpression;
-                if (count == _maxMemberExpressionTries)
-                    throw new ExpressionNotSupportedException(func);
-                count++;
+                body = (MemberExpression)ubody.Operand;
             }
             _dbProvider.AddOrUpdatePrimaryKey(typeof(TModel), body.Member.Name);
             return this;
